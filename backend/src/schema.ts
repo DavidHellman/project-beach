c97507b95a61b7340ee87edda87a84e03bc4f838
David Hellman <92davidhellman@gmail.com>
2022-07-06 20:31:27 +0200

    be setup

---
+++ backend/src/schema.ts
@@ -0,0 +1,109 @@
+import {
+  intArg,
+  makeSchema,
+  nonNull,
+  objectType,
+  stringArg,
+  inputObjectType,
+  arg,
+  asNexusMethod,
+  enumType,
+} from 'nexus'
+import { DateTimeResolver } from 'graphql-scalars'
+import { Context } from './context'
+
+export const DateTime = asNexusMethod(DateTimeResolver, 'date')
+
+const Query = objectType({
+  name: 'Query',
+  definition(t) {
+    t.nonNull.list.nonNull.field('allUsers', {
+      type: 'User',
+      resolve: (_parent, _args, context: Context) => {
+        return context.prisma.user.findMany()
+      },
+    })
+  },
+})
+
+const Mutation = objectType({
+  name: 'Mutation',
+  definition(t) {
+    t.nonNull.field('signupUser', {
+      type: 'User',
+      args: {
+        data: nonNull(
+          arg({
+            type: 'UserCreateInput',
+          }),
+        ),
+      },
+      resolve: (_, args, context: Context) => {
+        return context.prisma.user.create({
+          data: {
+            name: args.data.name,
+            email: args.data.email,
+          },
+        })
+      },
+    })
+  },
+})
+
+const User = objectType({
+  name: 'User',
+  definition(t) {
+    t.nonNull.int('id')
+    t.string('name')
+    t.nonNull.string('email')
+  },
+})
+
+const SortOrder = enumType({
+  name: 'SortOrder',
+  members: ['asc', 'desc'],
+})
+
+const UserUniqueInput = inputObjectType({
+  name: 'UserUniqueInput',
+  definition(t) {
+    t.int('id')
+    t.string('email')
+  },
+})
+
+const UserCreateInput = inputObjectType({
+  name: 'UserCreateInput',
+  definition(t) {
+    t.nonNull.string('email')
+    t.string('name')
+  },
+})
+
+export const schema = makeSchema({
+  types: [
+    Query,
+    Mutation,
+    User,
+    UserUniqueInput,
+    UserCreateInput,
+    SortOrder,
+    DateTime,
+  ],
+  outputs: {
+    schema: __dirname + '/../schema.graphql',
+    typegen: __dirname + '/generated/nexus.ts',
+  },
+  contextType: {
+    module: require.resolve('./context'),
+    export: 'Context',
+  },
+  sourceTypes: {
+    modules: [
+      {
+        module: '@prisma/client',
+        alias: 'prisma',
+      },
+    ],
+  },
+})
